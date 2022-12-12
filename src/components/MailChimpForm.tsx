817c9a6ea71f41329078fb16ea2e67231e5f3ac5
Aleksey Shingarev <alekseytim@mail.ru>
2022-12-12 13:06:54 +0300

    feat: contactForm

---
+++ src/components/MailChimpForm.tsx
@@ -0,0 +1,6 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+
+
+export const MailChimpForm = () => {
+  const postUrl = `${process.env.REACT_APP_MAIL_CHIMP_URL}?u=${process.env.REACT_APP_MAIL_CHIMP_U}$id=${process.env.REACT_APP_MAIL_CHIMP_ID}`
+}
