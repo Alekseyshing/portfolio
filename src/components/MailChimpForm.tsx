a462c47df5e5fd413f1289db7160000a9423dfad
Aleksey Shingarev <alekseytim@mail.ru>
2022-12-12 15:13:49 +0300

    feat: newsletter

--- src/components/MailChimpForm.tsx
+++ src/components/MailChimpForm.tsx
@@ -1,6 +1,23 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe"
-
+import MailchimpSubscribe, { EmailFormFields } from "react-mailchimp-subscribe"
+import { Newsletter } from "./Nawsletter"
 
 export const MailChimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAIL_CHIMP_URL}?u=${process.env.REACT_APP_MAIL_CHIMP_U}$id=${process.env.REACT_APP_MAIL_CHIMP_ID}`
+
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <Newsletter
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+          />
+        )}
+      />
+    </>
+  )
+
+
 }
---
+++ src/components/Nawsletter.tsx
@@ -0,0 +1,57 @@
+import { Col, Row, Alert } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { EmailFormFields } from "react-mailchimp-subscribe"
+
+interface INewsletter {
+  subscribe?: (data: EmailFormFields) => void,
+  onValidated: (data: EmailFormFields) => void,
+  status: "success" | "error" | "sending" | null,
+  message: string | Error | null
+}
+
+export const Newsletter = ({ onValidated, status, message }: INewsletter) => {
+  const [email, setEmail] = useState('');
+  useEffect(() => {
+    if (status === "success") clearFields()
+  }, [status])
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    email &&
+      email.indexOf('0') > -1 &&
+      onValidated({
+        EMAIL: email
+      })
+  }
+
+  const clearFields = () => {
+    setEmail('')
+  }
+
+  return (
+    <Col lg={12}>
+      <div className='newsletter-bx'>
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>Subscribe to our Newsletter</h3>
+            {status === 'sending' && <Alert>Sending...</Alert>}
+            {status === 'error' && <Alert variant='danger'>{message as string}</Alert>}
+            {status === 'success' && <Alert>{message as string}</Alert>}
+          </Col>
+          <Col md={6} xl={7}>
+            <form onSubmit={handleSubmit}>
+              <div className='new-email-bx'>
+                <input
+                  value={email}
+                  type='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder='Email Adress'
+                />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  )
+}
